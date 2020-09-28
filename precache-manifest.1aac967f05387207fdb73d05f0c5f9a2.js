89ed863ee775aee31b55b1c7f2c775271e6e3056
GisnetzEU <gismontero.mail@gmail.com>
2020-09-28 23:28:18 +0200

    Updates

---
+++ precache-manifest.1aac967f05387207fdb73d05f0c5f9a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bffacfb70070caa2fb05f71eaddc61",
+    "url": "/deletePersonHandler/index.html"
+  },
+  {
+    "revision": "151c45d9e3d58aa8af82",
+    "url": "/deletePersonHandler/static/css/main.d957a0c0.chunk.css"
+  },
+  {
+    "revision": "4692cee79e2973ed591e",
+    "url": "/deletePersonHandler/static/js/2.1fcd6ae1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deletePersonHandler/static/js/2.1fcd6ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151c45d9e3d58aa8af82",
+    "url": "/deletePersonHandler/static/js/main.9c02c21f.chunk.js"
+  },
+  {
+    "revision": "21feaed89d77dc91c7b1",
+    "url": "/deletePersonHandler/static/js/runtime-main.4f108e3c.js"
+  }
+]);
